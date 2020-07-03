2a7b835967b7bdf3dac3de1a610ca702f9a91527
Oscar Pachon <oscarpachon@seven4n.com>
2020-07-03 16:13:13 -0500

    Adding answer test

--- comandos-basicos/answer_1.sh
+++ comandos-basicos/answer_1.sh
@@ -2,5 +2,5 @@ This allows you to display additional information to help the user with the scen
 
 Such as, the correct command the user needs to run:
 
-`mkdir 2020/06/01`{{execute}}
+`mkdir -p 2020/06/01`{{execute}}
 `touch 2020/06/01/05_20_10.txt`{{execute}}
