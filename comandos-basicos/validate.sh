0ce8261e6ac3ee34737a601058e4407fa2f2f631
Oscar Pachon <oscarpachon@seven4n.com>
2020-07-03 10:23:41 -0500

    Testing scripts

--- comandos-basicos/validate.sh
+++ comandos-basicos/validate.sh
@@ -1,5 +1,5 @@
-echo -n "Ingresa tu comando y luego presiona enter: ";
-read;
+echo -n "Ingresa tu comando y luego presiona enter: " &> /dev/null
+read
 echo You typed ${REPLY}
 output=$(eval ${REPLY})
 echo "$output"
