3f70ae3390e79f28ff72494f41ae49934cf96752
Oscar Pachon <oscarpachon@seven4n.com>
2020-07-03 15:27:38 -0500

    Adding answer test

---
+++ comandos-basicos/answer_1.sh
@@ -0,0 +1,6 @@
+This allows you to display additional information to help the user with the scenario.
+
+Such as, the correct command the user needs to run:
+
+`mkdir 2020/06/01`{{execute}}
+`touch 2020/06/01/05_20_10.txt`{{execute}}
